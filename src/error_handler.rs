7fa1713ec383423993da85ff0af5bdcd4cc45bb4
Nakarin Kakanumporn <belizwp@live.com>
2021-11-17 12:18:16 +0700

    fix: json error handler explaination message

--- src/error_handler.rs
+++ src/error_handler.rs
@@ -9,8 +9,8 @@ use crate::actix_web_error::{ActixWebError, ErrorInfo};
 pub fn json_error_handler(err: JsonPayloadError, _req: &HttpRequest) -> Error {
     let awe = ActixWebError {
         error_info: ErrorInfo {
-            message: err.to_string(),
-            explaination: Some("Invalid json body".to_owned()),
+            message: "Invalid json body".to_string(),
+            explaination: Some(err.to_string()),
             action: None,
         },
         status_code: StatusCode::BAD_REQUEST,
